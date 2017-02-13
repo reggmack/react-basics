2445397a1a9cc605969835e7be6590f6fdbee388
reggmack <thegraphixninja@gmail.com>
2017-02-13 16:49:43 -0600

    Decomposing the App
    Break the app down into smaller components

--- app.jsx
+++ app.jsx
@@ -1,45 +1,62 @@
-function Application(props) {
+function Header (props) {
 	return (
-		<div className="scoreboard">
-			<div className="header">
-				<h1>{props.title}</h1>
+		<div className="header">
+			<h1>{props.title}</h1>
+		</div>	
+	);
+}
+
+Header.propTypes = {
+	title: React.PropTypes.string.isRequired,
+};
+
+
+function Counter(props) {
+	return (
+		<div className="counter">
+			<button className="counter-action decrement"> - </button>
+			<div className="counter-score"> {props.score} </div>
+			<button className="counter-action increment"> + </button>
+		</div>
+	);	
+}
+
+Counter.propTypes = {
+	score: React.PropTypes.number.isRequired,
+};
+
+function Player(props) {
+	return (
+		<div className="player">
+			<div className="player-name">
+				{props.name}
 			</div>
-			
-			<div className="players">
-				<div className="player">
-					<div className="player-name">
-					Regg Mack
-					</div>
-					<div className="player-score">
-						<div className="counter">
-							<button className="counter-action decrement"> - </button>
-							<div className="counter-score"> 31 </div>
-							<button className="counter-action increment"> + </button>
-						</div>
-					</div>
-				</div>
-				
-				<div className="player">
-					<div className="player-name">
-					Max Kilbane
-					</div>
-					<div className="player-score">
-						<div className="counter">
-							<button className="counter-action decrement"> - </button>
-							<div className="counter-score"> 33 </div>
-							<button className="counter-action increment"> + </button>
-						</div>
-					</div>
-				</div>
-				
+			<div className="player-score">
+				<Counter score={props.score} />
 			</div>
 		</div>
 	);
 }
 
-Application.propTypes = {
-	title: React.PropTypes.string,
+Player.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	score: React.PropTypes.number.isRequired,
 }
+
+function Application(props) {
+	return (
+		<div className="scoreboard">
+			<Header title={props.title} />
+			
+			<div className="players">
+				<Player name="Regg Mack" score={31} />
+				<Player name="Kilbane" score={33} />
+			</div>
+		</div>
+	);
+}
+
+
 
 Application.defaultProps = {
 	title: "Scoreboard",
