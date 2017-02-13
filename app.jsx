711f7a498b1c1e1a5132c857dc12bdfded5efe35
reggmack <thegraphixninja@gmail.com>
2017-02-13 12:25:01 -0600

    create a component
    create a Application component and call it

--- app.jsx
+++ app.jsx
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('container'));
+function Application() {
+	return (
+		<div>
+			<h1>Hello from React!</h1>
+			<p>I was rendered from the Application component</p>
+		</div>
+	);
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
